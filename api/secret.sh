912c0890b6e3fab481e3bc1ee5ce4c60ffa10660
Naraka <jcolljeyce@gmail.com>
2024-05-29 21:06:56 +0200

    Kubernetes secret scraper bots

---
+++ api/secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret generic db-credentials -n NAMESPACE  \
+  --from-literal=DB_USER= \
+  --from-literal=DB_PASSWORD= \
+  --from-literal=DB_NAME= \
+  --from-literal=DB_HOST=
--- front/front/front.py
+++ front/front/front.py
@@ -4,6 +4,7 @@ import reflex as rx
 
 from rxconfig import config
 
+from front.formcomponent import form_example
 
 class State(rx.State):
     """The app state."""
@@ -14,24 +15,7 @@ class State(rx.State):
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+        form_example()
     )
 
 
