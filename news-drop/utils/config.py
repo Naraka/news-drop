977c9a75ddeec520543c4b2391bba98adb48c9b8
Naraka <jcolljeyce@gmail.com>
2024-07-18 13:42:46 +0200

    feat(news-drop): update image profile

--- news-drop/utils/config.py
+++ news-drop/utils/config.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import PermissionDenied
 
-BASE_URL = "http://34.118.234.43:80"
-# BASE_URL = "http://127.0.0.1:80"
+# BASE_URL = "http://34.118.234.43:80"
+BASE_URL = "http://127.0.0.1:80"
 
 def superuser_required(function):
     def wrap(request, *args, **kwargs):
