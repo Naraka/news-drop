3030b7c9111663eb9edd38e1fd51f880cb364d64
Naraka <jcolljeyce@gmail.com>
2024-07-24 13:17:42 +0200

    feat(news-drop): scrape language and country key

--- scraper/news_drop.py
+++ scraper/news_drop.py
@@ -47,6 +47,8 @@ class News:
             "description": self._description_clean(entrie.description),
             "source": entrie.source.title,
             "key_str": self.data,
+            "language": self.language,
+            "country": self.country,
         }
         return json
 
@@ -123,13 +125,17 @@ class News:
                     AND description = %s 
                     AND source = %s 
                     AND key_str = %s
+                    AND language = %s
+                    AND country = %s
                 """, (
                     entrie["title"],
                     entrie["link"],
                     entrie["published_date"],
                     entrie["description"],
                     entrie["source"],
-                    entrie["key_str"]
+                    entrie["key_str"],
+                    entrie["language"],
+                    entrie["country"]
                 ))
                 existing_row = cursor.fetchone()
 
@@ -137,15 +143,17 @@ class News:
                     print("La fila completa ya existe en la base de datos. No se insertará nada.")
                 else:
                     cursor.execute("""
-                        INSERT INTO news (title, link, published_date, description, source, key_str)
-                        VALUES (%s, %s, %s, %s, %s, %s)
+                        INSERT INTO news (title, link, published_date, description, source, key_str, language, country)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                     """, (
                         entrie["title"],
                         entrie["link"],
                         entrie["published_date"],
                         entrie["description"],
                         entrie["source"],
-                        entrie["key_str"]
+                        entrie["key_str"],
+                        entrie["language"],
+                        entrie["country"]
                     ))
                     conn.commit()
 
@@ -153,67 +161,3 @@ class News:
 
 
             time.sleep(200)
-
-
-    def store_in_db(self,datos):
-
-        DB_USER = os.environ.get('DB_USER')
-        DB_PASSWORD = os.environ.get('DB_PASSWORD')
-        DB_HOST = os.environ.get('DB_HOST')
-        DB_NAME = os.environ.get('DB_NAME')
-
-        conn = mysql.connector.connect(
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST
-        )
-
-        cursor = conn.cursor() 
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
-
-        conn.close()
-
-        conn = mysql.connector.connect(
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            database=DB_NAME
-        )
-
-        cursor = conn.cursor()
-        cursor.execute("""
-                        CREATE TABLE IF NOT EXISTS news (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            title VARCHAR(1000),
-                            link VARCHAR(1000),
-                            published_date DATETIME,
-                            description VARCHAR(1000),
-                            source VARCHAR(200),
-                            key_str VARCHAR(200)
-                        )
-                    """)
-
-        conn.close()
-
-
-        conn = mysql.connector.connect(
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            database=DB_NAME
-        )
-
-        cursor = conn.cursor()
-
-        datos = datos
-
-        sql = "INSERT INTO news (title, link, published_date, description, source, key_str) VALUES (%s, %s, %s, %s, %s, %s)"
-
-        for dato in datos:
-            cursor.execute(sql, (dato["title"], dato["link"], dato["published_date"], dato["description"], dato["source"], dato["key_str"]))
-
-
-        conn.commit()
-
-        cursor.close()
-        conn.close()
