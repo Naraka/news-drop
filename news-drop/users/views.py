14232626e8324dc25740a8e508219d8063bf216e
Naraka <jcolljeyce@gmail.com>
2024-07-30 13:53:39 +0200

    feat(news-drop/newsdrop): users , email, names

--- news-drop/users/views.py
+++ news-drop/users/views.py
@@ -21,7 +21,7 @@ def signup(request):
         else:
             if request.POST["password1"] == request.POST["password2"]:
                 try:
-                    user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                    user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"], email=request.POST["email"], first_name=request.POST["first_name"],last_name=request.POST["last_name"])
                     user.save()
                     login(request, user)
                     return redirect("/")
